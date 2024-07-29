ff5cb85168dab0959016c318120bbfb3f532b804
FailArive <philip.luebeck@gmx.de>
2024-07-29 18:27:41 +0000

    commit on "main"

--- examples/with-tailwindcss/astro.config.mjs
+++ examples/with-tailwindcss/astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import icon from "@iconify-json/mdi"
+
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	integrations: [icon()],
+	integrations: [
+    icon({
+      include: {
+        mdi: ["*"], // (Default) Loads entire Material Design Icon set
+        mdi: ["account"], // Loads only Material Design Icon's "account" SVG
+      },
+    }),
+  ],
 });
