4086fc45db3d1adcc900d789cd2b4ebed706e137
FailArive <philip.luebeck@gmx.de>
2024-08-05 23:06:38 +0000

    commit on "main"

--- examples/with-tailwindcss/src/js/cardanimation.js
+++ examples/with-tailwindcss/src/js/cardanimation.js
@@ -4,12 +4,14 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('show');          
         } else {
             entry.target.classList.remove('show');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElementsLeft = document.querySelectorAll('.hidden-left');
+hiddenElementsLeft.forEach((el) =>  observer.observe(el));
+const hiddenElementsRight = document.querySelectorAll('.hidden-right');
+hiddenElementsRight.forEach((el) =>  observer.observe(el));
