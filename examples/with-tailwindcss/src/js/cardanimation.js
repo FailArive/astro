3efe9922184094b094b1fc9ad2e6c214b0884d63
FailArive <philip.luebeck@gmx.de>
2024-11-03 20:08:18 +0000

    commit on "main"

--- examples/with-tailwindcss/src/js/cardanimation.js
+++ examples/with-tailwindcss/src/js/cardanimation.js
@@ -1,7 +1,8 @@
 import './../css/style.scss'
 
 const videos = document.querySelectorAll(".latestvideo");
-
+const lcarden = document.querySelectorAll(".hidden_left");
+const rcarden = document.querySelectorAll(".hidden_right");
 
 window.addEventListener("scroll", ()=>{
     const innerHeightOfWindow = window.innerHeight;
@@ -13,6 +14,19 @@ window.addEventListener("scroll", ()=>{
             latestvideo.classList.add("show");
         }
     });
-    
+    lcarden.forEach(hidden_left =>{
+        const lcards = hidden_left.getBoundingClientRect().top;
 
+        if(lcards < innerHeightOfWindow){
+            hidden_left.classList.add("show");
+        }
+    });
+    rcarden.forEach(hidden_right =>{
+        const rcards = hidden_right.getBoundingClientRect().top;
+
+        if(rcards < innerHeightOfWindow){
+            hidden_right.classList.add("show");
+        }
+    });
 });
+
