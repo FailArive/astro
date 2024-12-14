1bf5a7dcfbc7cae2b616d31a8f3984d6e75ee63b
FailArive <philip.luebeck@gmx.de>
2024-12-14 20:53:42 +0000

    commit on "main"

--- examples/with-tailwindcss/src/js/searchfunction.js
+++ examples/with-tailwindcss/src/js/searchfunction.js
@@ -1,16 +1,16 @@
-const search = () =>{
+window.search = () =>{
     const searchBox = document.getElementById("search-item").value.toUpperCase();
 
-    const storeitems = document.getElementById("product-list");
+    const storeitems = document.getElementById("productl")
 
-    const product = document.querySelectorAll(".product");
-    const pname = storeitems.getElementsByTagName("h2");
+    const product = document.querySelectorAll(".product")
+    const pname = storeitems.getElementsByTagName("h2")
 
     for (var i= 0; i < pname.length; i++) {
         let match = product[i].getElementsByTagName('h2')[0];
 
         if(match){
-            let textvalue = match.textContent || match.innerHTML;
+            let textvalue = match.textContent || match.innerHTML
 
             if(textvalue.toUpperCase().indexOf(searchBox) > -1){
                 product[i].style.display = "";
